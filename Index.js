7ad6ac9fc50a33def8ecded0d4a14c70c2bbc09f
lucasA49 <lucasaksu@gmail.com>
2024-03-18 12:23:16 +0100

    normalementok

---
+++ Index.js
@@ -0,0 +1,3 @@
+const userInformation = require('./information');
+console.log(`Hi, my name is lucas and I'm in wcschool nantes`);
+const cowsay = require("cowsay");
---
+++ information.js
@@ -0,0 +1,14 @@
+const name= "lucas aksu";
+const campus= "Wcschool nantes";
+
+module.exports = {
+  name:"lucas aksu",
+  campus:"WCschool nantes",
+};
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "Hi, my name is lucas in wcschool nantes",
+    e : "oO",
+    T : "U "
+}));
